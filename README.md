e314a5720046c19a3af9787da595fd886871ecec
EugenioBarbieriViale <eugenio.barbieri.viale@gmail.com>
2025-04-24 23:58:33 +0200

    new

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # physiks
+
+- Single math symbols: https://www.kaggle.com/datasets/xainano/handwrittenmathsymbols
+- Full expressions: https://www.kaggle.com/datasets/shahrukhkhan/im2latex100k
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from matplotlib.pyplot import imshow, show
 import cv2 as cv
 
-image = cv.imread("t.jpg")
+# image = cv.imread("t.jpg")
+image = cv.imread("test.png")
 image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
-image = cv.GaussianBlur(image, (5,5), 0)
-# image = cv.adaptiveThreshold(image, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
 # image = cv.Canny(image, 100, 200)
-th, image = cv.threshold(image, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+# image = cv.adaptiveThreshold(image, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
+
+# image = cv.GaussianBlur(image, (5, 5), 0)
+# th, image = cv.threshold(image, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
 
 contours, hierarchy = cv.findContours(image, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
 # cv.drawContours(image, contours, -1, (0, 255, 0), 2)
